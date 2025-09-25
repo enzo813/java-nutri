66a7551e4c9e9b10d42b70c6eec4ab0632584f07
enzo813 <gaspar.enzo@escola.pr.gov.br>
2025-09-25 07:59:42 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -4,9 +4,10 @@ subtitulo.textContent = "Meus Pacientes";
 var Enzin = document.querySelector(".titulo");
 Enzin.textContent = "Enzin nutriçao";
 //acessar a tag tr
-var paciente =document.querySelectorAll(".paciente");
+var Pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0 ; i < paciente.length; i++){
+for(var i = 0 ; i < Pacientes.length; i++){
+ var paciente = Pacientes [i];
    //seleciona o peso
 var tdpeso = paciente.querySelector(".info-peso");
 var peso = tdpeso.textContent;
@@ -24,11 +25,12 @@ if(pesoValido && alturaValida){
 console.log ("altura invalida");
         // acessa e altera o imc
 var tdimc = paciente.querySelector(".info-imc");
-tdimc.textContent = imc;
- }if(peso < 0 || peso > 1000){
-console.log ("peso invalido");
+tdimc.textContent = imc.toFixed(2);
+ }
+ if(peso < 0 || peso > 1000){
 var pesoValido = false;
 tdimc.textContent = "peso invalido"
+paciente.style.backgroundColor = "red"
 }
 if(altura <= 0 || altura >= 3.00){
 var alturaValida = false;
