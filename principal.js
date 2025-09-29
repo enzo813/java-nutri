dd0f92ae84dccec5cb95d8f29ee4db8500092511
enzo813 <gaspar.enzo@escola.pr.gov.br>
2025-09-29 11:07:58 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -30,12 +30,21 @@ tdimc.textContent = imc.toFixed(2);
  if(peso < 0 || peso > 1000){
 var pesoValido = false;
 tdimc.textContent = "peso invalido"
-paciente.style.backgroundColor = "red"
+paciente.classList.add("campo-invalido");
 }
 if(altura <= 0 || altura >= 3.00){
 var alturaValida = false;
 tdimc.textContent = "altura invalida";
+paciente.classList.add("campo-invalido");
 }
 }
+//quando clicar no titulo, apareça uma mensagem
+Enzin
+.addEventListener('click', mostraMensagem);
+
+function mostraMensagem(){
+        alert("Este elemento foi clicado");
+}
+
 
 
